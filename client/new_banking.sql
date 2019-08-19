0ab8429ecd73540a9d3cc038003e0e60f00e3c9d
Joel Mellbin <joel.mellbin@edu.sandviken.se>
2019-08-19 15:19:32 +0200

    first commit

---
+++ client/new_banking.sql
@@ -0,0 +1,12 @@
+USE `fivem2337`;
+
+CREATE TABLE `loans` (
+
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  "loan" int(11) NOT NULL,
+  "creditValue" INT(11) NOT NULL,
+
+  PRIMARY KEY (`id`)
+);
---
+++ server.lua
@@ -0,0 +1,102 @@
+--================================================================================================
+--==                                VARIABLES - DO NOT EDIT                                     ==
+--================================================================================================
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('bank:deposit')
+AddEventHandler('bank:deposit', function(amount)
+	local _source = source
+
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	if amount == nil or amount <= 0 or amount > xPlayer.getMoney() then
+		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+	else
+		xPlayer.removeMoney(amount)
+		xPlayer.addAccountMoney('bank', tonumber(amount))
+	end
+end)
+
+
+RegisterServerEvent('bank:withdraw')
+AddEventHandler('bank:withdraw', function(amount)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local base = 0
+	amount = tonumber(amount)
+	base = xPlayer.getAccount('bank').money
+	if amount == nil or amount <= 0 or amount > base then
+		TriggerClientEvent('chatMessage', _source, "Invalid Amount")
+	else
+		xPlayer.removeAccountMoney('bank', amount)
+		xPlayer.addMoney(amount)
+	end
+end)
+
+RegisterServerEvent('bank:balance')
+AddEventHandler('bank:balance', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	balance = xPlayer.getAccount('bank').money
+	TriggerClientEvent('currentbalance1', _source, balance)
+
+end)
+-- hämtar nuvarande lån
+RegisterServerEvent("bank:getloan")
+AddEventHandler("bank:getloan", function(id, cb))
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+				MySQL.async.fetchALL(
+				"select * FROM loans WHERE identifier = @identifier", {
+					["@identifier"] = xPlayer.identifier
+				},
+				function result (result)
+				--this needs help
+					local loans = {}
+
+						for i=1, #result, 1 do
+							TABLE.insert(loans ,{
+								id = result[i].id
+								identifier = result[i].identifier
+								amount = result[i].amount
+								loan = result[i].loan
+								creditValue = result[i].creditValue
+						})
+					end
+					cb(result)
+				end
+			)
+
+RegisterServerEvent("bank:loan")
+AddEventHandler("bank:loan", function(amountl))
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local loanBalance = xPlayer.getAccount("bank").loan
+
+
+
+
+s
+RegisterServerEvent('bank:transfer')
+AddEventHandler('bank:transfer', function(to, amountt)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local zPlayer = ESX.GetPlayerFromId(to)
+	local balance = 0
+	balance = xPlayer.getAccount('bank').money
+	zbalance = zPlayer.getAccount('bank').money
+
+	if tonumber(_source) == tonumber(to) then
+		TriggerClientEvent('chatMessage', _source, "You cannot transfer to your self")
+	else
+		if balance <= 0 or balance < tonumber(amountt) or tonumber(amountt) <= 0 then
+			TriggerClientEvent('chatMessage', _source, "You don't have enough money in the bank.")
+		else
+			xPlayer.removeAccountMoney('bank', amountt)
+			zPlayer.addAccountMoney('bank', amountt)
+		end
+
+	end
+end)
